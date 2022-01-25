88e5f9c50a4e66ee912401f062da66bb46490444
Valentin Long <valentin.long@uha.fr>
2022-01-25 16:56:26 +0100

    FusionSemiFinale

--- netflix/Projets/Valentin/Fusion/main.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-with open('output.csv', 'w', newline='', encoding="utf8") as output:
-    for i in range(1, 6):
-        with open(f'../../../data/raw/netflix-{i}.csv', newline='', encoding="utf8") as csvFile:
-            for row in csvFile:
-                ecriture = csv.writer(output)
-                ecriture.writerow(csvFile)
-
