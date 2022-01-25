103d3c5f7ecefff62a0f2034ebe8fb596ae7bf1b
olivier <olivierlm25@gmail.com>
2022-01-25 16:31:32 +0100

    fusion

--- netflix/Projets/olivier/olivierv2.py
+++ netflix/Projets/olivier/olivierv2.py
@@ -1,14 +1,19 @@
+import pandas as pds
 import pandas as pd
-import glob
-import os
+from pandas import DataFrame
 
-joined_files = os.path.join("netflix-*.csv")
+#output = pd.concat(
+    #map(pd.read_csv, ['netflix-1.csv', 'netflix-2.csv','netflix-3.csv','netflix-4.csv','netflix-5.csv' ]), ignore_index=True)
 
-joined_list = glob.glob(joined_files)
-
-# Finally, the files are joined
-df = pd.concat(map(pd.read_csv, joined_list), ignore_index=True)
-print(df)
-
-output=(df)
-output.to_csv("test.csv",index=False)
+#output.to_csv("fusion.csv", index=False)
+#df = DataFrame.from_items(
+ #   orient='index',
+  #  columns=['show_id','type','title','director','cast','country','date_added','release_year','rating','duration','listed_in','description'])
+#del df['description']
+for i in range (1,6):
+    lecture= pds.read_csv(f"netflix-{i}.csv", header=None)
+    test = lecture.drop(lecture.columns[11], axis=1)
+    test.to_csv(f"output{i}.csv")
+df = pd.concat(
+    map(pd.read_csv, ['output1.csv', 'output2.csv','output3.csv','output4.csv','output5.csv' ]), ignore_index=True)
+df.to_csv("fusion.csv", index=False)
