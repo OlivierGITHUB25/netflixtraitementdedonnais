e55fdc0957f12879d10579118975dd33286ed155
Valentin Long <valentin.long@uha.fr>
2022-01-26 11:58:21 +0100

    progPourcentage

--- netflix/Projets/Valentin/code/valeurAberantes.py
+++ netflix/Projets/Valentin/code/valeurAberantes.py
@@ -1,38 +1,45 @@
-import pandas
 import pandas as pd
 
 read = pd.read_csv('fusion.csv', index_col=False)
-valeurManquante = 0
-valeurAberante = 0
-
+vaManquante = 0
+vaAberante = 0
+vaManquanteCol = 0
+delList = []
 
 for i in range(1, 8808):
-    added = read.iloc[i, 6]
-    released = int(read.iloc[i, 7])
     try:
+        added = read.iloc[i, 6]
+        released = int(read.iloc[i, 7])
         nothing, added = added.split(",")
         added = int(added)
     except:
-        print(f"ERREUR VALEUR s{i}")
-        valeurManquante += 1
-
+        delList.append(i)
+        vaManquante += 1
+        vaManquanteCol += 1
     if added < released:
-        valeurAberante += 1
-        print(added, released, f"--> Erreur Ligne s{i}")
+        vaAberante += 1
+        print(added, released, f"--> Valeur Abérante Ligne s{i}")
+        delList.append(i)
+    for n in range(0, 11):
+        cellCheck = pd.isnull(read.iloc[i, n])
+        if cellCheck == True:
+            delList.append(i)
+            vaManquante += 1
 
-
-valeurManquante_ = valeurManquante * 100 / 8808
-valeurAberante_ = valeurAberante * 100 / 8808
-total = valeurAberante + valeurManquante
-total_ = total * 100 / 8808
-
+calc = lambda x : x * 100 / 8808
+read.drop(read.index[delList], inplace=True)
+read.to_csv('FusionCorrigée.csv', index=False)
+total = vaAberante + vaManquante
 
 print(f"--------------------------------\n"
-      f"NOMBRE VALEURS MANQUANTES : {valeurManquante}\n"
-      f"Pourcentage : {valeurManquante_:.2f}\n"
+      f"NOMBRE VALEURS MANQUANTES TOTALES : {vaManquante}\n"
+      f"Pourcentage : {calc(vaManquante):.2f}\n"
       f"--------------------------------\n"
-      f"NOMBRE VALEURS ABERANTES : {valeurAberante}\n"
-      f"Pourcentage : {valeurAberante_:.2f}\n"
+      f"NOMBRE VALEURS MANQUANTES (Col 6 et 7): {vaManquanteCol}\n"
+      f"Pourcentage : {calc(vaManquanteCol):.2f}\n"
+      f"--------------------------------\n"
+      f"NOMBRE VALEURS ABERANTES (Col 6 et 7): {vaAberante}\n"
+      f"Pourcentage : {calc(vaAberante):.2f}\n"
       f"--------------------------------\n"
       f"Total d'erreur : {total}\n"
-      f"Pourcentage d'erreur total : {total_:.2f}")
+      f"Pourcentage d'erreur total : {calc(total):.2f}")
