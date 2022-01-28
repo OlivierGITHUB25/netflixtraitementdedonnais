e3bb0cb14b62ea13ca63536bdc6877d24315a75e
Valentin Long <valentin.long@uha.fr>
2022-01-28 08:58:36 +0100

    graph

--- netflix/src/data/grapheF.py
+++ netflix/src/data/grapheF.py
@@ -3,36 +3,30 @@ import matplotlib.pyplot as plt
 import re
 
 readCSV = pd.read_csv('../../output/finalMerge.csv', index_col=False)
-TVCounter = 0
 counter = 0
 listeTV=[]
 listeSaison=[]
 listeSaisonM=[]
 total = 0
 conteur=0
-for i in range(1900, 2050):
-    while True:
-        try:
-            cellCheck = pd.isnull(readCSV.iloc[counter, 1])
-        except IndexError:
-            break
-        if cellCheck == False:
-            series = readCSV.iloc[counter, 1]
-            print("test0")
-            if series == "TV Show":
-                cellCheck = pd.isnull(readCSV.iloc[counter, 7])
-                TVCounter += 1
-                print("test1")
-                if cellCheck == False:
+dateListe = []
+
+while True:
+    try:
+        CellCheck = pd.isnull(readCSV.iloc[counter, 1])
+    except IndexError:
+        break
+    if CellCheck == False:
+        series = readCSV.iloc[counter, 1]
+        if series == "TV Show":
+            CellCheck = pd.isnull(readCSV.iloc[counter, 7])
+            if CellCheck == False:
+                for i in range(1900, 2051):
                     series = readCSV.iloc[counter, 7]
-                    print("test2")
-                    i = str(f"{i}")
-                    if series == "2020":
+                    if series == str(i):
                         conteur += 1
-                        print("test3")
                         TV = readCSV.iloc[counter, 9]
-                        number, bin = TV.split(" ")
-                        total+=int(re.findall('\d+', f'{number}')[0] )
+                        total += int(re.findall('\d+', f'{TV}')[0] )
 
 
         counter += 1
