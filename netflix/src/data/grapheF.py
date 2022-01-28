86a3c4798775ccd8e118aa19547fc7b19393298f
Olivier Lamontagne <olivier.lamontagne@uha.fr>
2022-01-28 09:29:04 +0100

    version-graphe-sans-pandas-pas-fini

--- netflix/src/data/grapheF.py
+++ netflix/src/data/grapheF.py
@@ -1,16 +1,23 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import re
+import csv
 
-readCSV = pd.read_csv('../../output/finalMerge.csv', index_col=False)
+#readCSV = pd.read_csv('../../output/finalMerge.csv', index_col=False)
+with open('../../output/finalMerge.csv','r') as file:
+    reader = list(csv.reader(file))
+    for i in range(len(reader)):
+        print(reader[i])
+
 counter = 0
+ligne=0
 listeTV=[]
 listeSaison=[]
 listeSaisonM=[]
 total = 0
 conteur=0
 dateListe = []
-
+'''
 while True:
     try:
         CellCheck = pd.isnull(readCSV.iloc[counter, 1])
@@ -21,9 +28,10 @@ while True:
         if series == "TV Show":
             CellCheck = pd.isnull(readCSV.iloc[counter, 7])
             if CellCheck == False:
-                for i in range(1900, 2051):
-                    series = readCSV.iloc[counter, 7]
+                for i in range(1900, 2050):
+                    series = readCSV.iloc[ligne, 7]
                     if series == str(i):
+                        ligne+=1
                         conteur += 1
                         TV = readCSV.iloc[counter, 9]
                         total += int(re.findall('\d+', f'{TV}')[0] )
@@ -32,4 +40,4 @@ while True:
         counter += 1
 total=total/conteur
 listeSaison.append(total)
-print(f"{listeSaison},{total},{conteur}")
+print(f"{listeSaison},{total},{conteur}")'''
